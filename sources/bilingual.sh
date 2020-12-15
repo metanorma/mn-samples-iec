32ecd756c67db4c3763e4db75e0d2cd9b4ea83fd
Nick Nicholas <opoudjis@gmail.com>
2020-12-16 01:21:56 +1100

    add French and bilingual

---
+++ sources/bilingual.sh
@@ -0,0 +1,3 @@
+bundle exec metanorma iec-rice-en.adoc
+bundle exec metanorma iec-rice-fr.adoc
+bundle exec metanorma collection bilingual.yml -x xml,html,doc,presentation,pdf -w bilingual -c collection_cover.html
